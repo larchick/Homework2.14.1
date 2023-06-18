a511075c7b4e6a26fdf6671917d13c87cc51971a
larchick <liliyagreis@gmail.com>
2023-06-19 00:20:34 +0300

    2

---
+++ src/main/java/com/example/demotesting/service/DepartmentService.java
@@ -0,0 +1,15 @@
+package com.example.demotesting.service;
+
+
+import com.example.demotesting.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public interface DepartmentService {
+     Employee getEmployeeWithMaxSalary(Integer departmentId);
+    Employee getEmployeeWithMinSalary(Integer departmentId);
+    Map<Integer, List<Employee>> getGroupedByDepartmentEmployees(Integer departmentId);
+}
