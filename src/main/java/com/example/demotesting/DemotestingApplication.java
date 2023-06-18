30e78928f0688713c1eab68c19bef0d6042b9541
larchick <liliyagreis@gmail.com>
2023-06-18 23:34:20 +0300

    1

---
+++ src/main/java/com/example/demotesting/DemotestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demotesting/DemotestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
